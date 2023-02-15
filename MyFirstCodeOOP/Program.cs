993c5d227e9075ca7fe183d39350a38e5d268b2b
JHOJAN FELIPE TUBERQUIA PINO <jhojantuberquia301173@correo.itm.edu.co>
2023-02-15 17:41:03 -0500

    I created my first code with a new Date Class

---
+++ MyFirstCodeOOP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyFirstCodeOOP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var dateObject = new Date(1899, 2, 12);
+                Console.WriteLine(dateObject);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
